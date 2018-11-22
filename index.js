f2bcfa261f898f8c1e051427c71cbbd5fafa8072
Jean-Christophe Martel <tramkotipo_18@hotmail.com>
2018-11-22 13:00:30 -0500

    Added APIs and started client

--- index.js
+++ index.js
@@ -1,10 +1,48 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
 const app = express()
-const db = require('./db')
+const db = require('./scripts/db')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+function getInvoices() {
+    return db.mongoose.model('invoices').find({})
+}
+
+function addInvoice(body) {
+    let Invoice = db.mongoose.model('invoices')
+    let invoice = new Invoice()
+
+    invoice.id = body.id
+    invoice.products = body.products
+
+    return invoice
+}
+
+function getProducts() {
+    return db.mongoose.model('products').find({})
+}
+
+function getProduct(productId) {
+    return db.mongoose.model('products').find({id: productId})
+}
+
+function addProduct(body) {
+    let Product = db.mongoose.model('products')
+    let product = new Product()
+
+    product.id = body.id
+    product.name = body.name
+    product.price = body.price
+
+    return product
+}
 
 app.get('/', function (req, res) {
-    db.mongoose.model("Invoice").find({}).then(data => {
-      res.send(data)
+    getInvoices().then((invoices) => {
+        res.json(invoices);
     })
 })
 
@@ -12,18 +50,38 @@ app.listen(3009, function () {
     console.log('Example app listening on port 3009!')
 })
 
-app.get('/api/factures', (req, res) => {
-
+app.get('/api/invoices', (req, res) => {
+    getInvoices().then((invoices) => {
+        res.json(invoices);
+    })
 })
 
-app.post('/api/factures', (req, res) => {
-
+app.post('/api/invoices', (req, res) => {
+    addInvoice(req.body).save((err) => {
+        if (err) {
+            res.status(400)
+            res.send(err)
+        } else {
+            res.status(201)
+            res.send('The invoice has been created')
+        }
+    })
 })
 
 app.get('/api/products', (req, res) => {
-
+    getProducts().then((products) => {
+        res.json(products);
+    })
 })
 
 app.post('/api/products', (req, res) => {
-    
+    addProduct(req.body).save((err) => {
+        if (err) {
+            res.status(400)
+            res.send(err)
+        } else {
+            res.status(201)
+            res.send('The product has been created')
+        }
+    })
 })
