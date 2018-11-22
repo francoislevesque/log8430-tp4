4cf162bc4b1285af643acfa8f2207d07406089da
Jean-Christophe Martel <tramkotipo_18@hotmail.com>
2018-11-22 10:06:28 -0500

    add mongoose

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.listen(3009, function () {
-  console.log('Example app listening on port 3009!')
+    console.log('Example app listening on port 3009!')
+})
+
+app.get('/api/factures', (req, res) => {
+
+})
+
+app.post('/api/factures', (req, res) => {
+
+})
+
+app.get('/api/products', (req, res) => {
+
+})
+
+app.post('/api/products', (req, res) => {
+    
 })
