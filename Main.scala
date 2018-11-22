d03f74123cc5f2c83ef5f73b7e07526a3a41ef39
francoislevesque <francois@witify.io>
2018-11-22 11:45:29 -0500

    Starting

--- Main.scala
+++ Main.scala
@@ -1,40 +1,21 @@
-import com.mongodb.spark._
-import com.mongodb.spark.config._
-import com.mongodb.spark.sql._
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.{max, min}
-import org.bson.Document
+package com.mongodb
 
-object Main extends App {
-  val spark = SparkSession.builder()
-    .appName("log8430-tp4")
-    .master("local[*]")
-    .getOrCreate()
+object GettingStarted {
 
-  // Read the data from MongoDB to a DataFrame
-  val readConfig = ReadConfig(Map("uri" -> "mongodb://127.0.0.1:27017/", "database" -> "log8430-tp4", "collection" -> "invoices"))
-  val data = spark.read.mongo(readConfig)
-  
-  data.printSchema()
-  data.show()
-  println(data.count)
+  def main(args: Array[String]): Unit = {
 
-  /*val transactions: RDD[Array[String]] = data.map(s => s.trim.split(' '))
+    /* Create the SparkSession.
+     * If config arguments are passed from the command line using --conf,
+     * parse args for the values to set.
+     */
+    import org.apache.spark.sql.SparkSession
 
-  val fpg = new FPGrowth()
-    .setMinSupport(0.2)
-    .setNumPartitions(10)
-  val model = fpg.run(transactions)
+    val spark = SparkSession.builder()
+      .master("local")
+      .appName("MongoSparkConnector")
+      .config("spark.mongodb.input.uri", "mongodb://admin:secret@0.0.0.0:27017/log8430-tp4.products")
+      .config("spark.mongodb.output.uri", "mongodb://admin:secret@0.0.0.0:27017/log8430-tp4.frequent_products")
+      .getOrCreate()
 
-  model.freqItemsets.collect().foreach { itemset =>
-    println(s"${itemset.items.mkString("[", ",", "]")},${itemset.freq}")
   }
-
-  val minConfidence = 0.8
-  model.generateAssociationRules(minConfidence).collect().foreach { rule =>
-    println(s"${rule.antecedent.mkString("[", ",", "]")}=> " +
-      s"${rule.consequent .mkString("[", ",", "]")},${rule.confidence}")
-  }*/
-
-
 }
