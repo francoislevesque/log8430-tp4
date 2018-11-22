6d030037157ebcf91e819d82b8cf675530471430
francoislevesque <francois@witify.io>
2018-11-22 12:22:29 -0500

    v2

--- Main.scala
+++ Main.scala
@@ -1,6 +1,8 @@
 import com.mongodb._
 import com.mongodb.spark._
+import com.mongodb.spark.sql._
 import org.bson.Document
+import com.mongodb.spark.config._
 
 object GettingStarted {
 
@@ -15,14 +17,14 @@ object GettingStarted {
     val spark = SparkSession.builder()
       .master("local")
       .appName("MongoSparkConnector")
-      .config("spark.mongodb.input.uri", "mongodb://admin:secret@0.0.0.0:27017/log8430-tp4.products")
-      .config("spark.mongodb.output.uri", "mongodb://admin:secret@0.0.0.0:27017/log8430-tp4.frequent_products")
+      .config("spark.mongodb.input.uri", "mongodb://admin:secret@0.0.0.0:27017/log8430-tp4")
+      .config("spark.mongodb.input.collection", "invoices")
+      //.config("spark.mongodb.output.uri", "mongodb://admin:secret@0.0.0.0:27017/log8430-tp4.frequent_products")
       .getOrCreate()
 
-    val readConfig = ReadConfig(Map("uri" -> "mongodb://admin:secret@0.0.0.0:27017/log8430-tp4.products", "readPreference.name" -> "secondaryPreferred"))
     val df = MongoSpark.load(spark)
 
-    println(df.count)
+    println(df.show())
     //println(rdd.first.toJson)
   }
 }
