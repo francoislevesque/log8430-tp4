731dd97d3084e8ae4d315f21281236548d5fbf78
cumuluswebdesign <info@cumuluswebdesign.com>
2018-11-22 09:37:14 -0500

    Port 3009

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+app.set('port', 3009);
+
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function(req, res) {
   res.send('hello world');
