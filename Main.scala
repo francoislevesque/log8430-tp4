53f2ceb2f373ef457eabb9628ef446ca3fc655cf
francoislevesque <francois@witify.io>
2018-11-22 12:35:45 -0500

    New in code config

--- Main.scala
+++ Main.scala
@@ -17,9 +17,8 @@ object GettingStarted {
     val spark = SparkSession.builder()
       .master("local")
       .appName("MongoSparkConnector")
-      .config("spark.mongodb.input.uri", "mongodb://admin:secret@162.243.164.88:27017/log8430-tp4")
-      .config("spark.mongodb.input.collection", "invoices")
-      //.config("spark.mongodb.output.uri", "mongodb://admin:secret@162.243.164.88:27017/log8430-tp4.frequent_products")
+      .config("spark.mongodb.input.uri", "mongodb://127.0.0.1:27017/log8430-tp4.products?readPreference=primaryPreferred")
+      .config("spark.mongodb.output.uri", "mongodb://127.0.0.1:27017/log8430-tp4.frequent_products")
       .getOrCreate()
 
     val df = MongoSpark.load(spark)
