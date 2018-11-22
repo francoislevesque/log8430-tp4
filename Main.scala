1b735a4224eb25217466af5f8f3bfe442fae8083
François Lévesque <francois@witify.io>
2018-11-22 13:44:03 -0500

    Use only raw invoice information

--- Main.scala
+++ Main.scala
@@ -4,6 +4,7 @@ import com.mongodb.spark.sql._
 import org.bson.Document
 import com.mongodb.spark.config._
 import org.apache.spark.rdd.RDD
+import org.apache.spark.mllib.fpm.FPGrowth
 
 object GettingStarted {
 
@@ -18,19 +19,31 @@ object GettingStarted {
     val spark = SparkSession.builder()
       .master("local")
       .appName("MongoSparkConnector")
-      .config("spark.mongodb.input.uri", "mongodb://127.0.0.1:27017/log8430-tp4.products?readPreference=primaryPreferred")
+      .config("spark.mongodb.input.uri", "mongodb://127.0.0.1:27017/log8430-tp4.invoices?readPreference=primaryPreferred")
       .config("spark.mongodb.output.uri", "mongodb://127.0.0.1:27017/log8430-tp4.frequent_products")
       .getOrCreate()
 
     // Load products table into data frame.
     val df = MongoSpark.load(spark)
 
-    // Print contents of products table.
-    println(df.show())
+    // Select only raw information on number of products per invoice.
+    val dfInvoices = df.select("products")
 
     // Convert data fram to resilient distributed dataset (RDD)
-    val rows: org.apache.spark.rdd.RDD[org.apache.spark.sql.Row] = df.rdd
-    rows.collect().foreach(println)
+    val invoices: org.apache.spark.rdd.RDD[org.apache.spark.sql.Row] = dfInvoices.rdd
+    invoices.collect().foreach(println)
+
+    // FPG
+/*
+    val fpg = new FPGrowth()
+        .setMinSupport(0.2)
+        .setNumPartitions(10)
+    val model = fpg.run(invoices)
+
+    model.freqItemsets.collect().foreach { itemset =>
+        println(s"${itemset.items.mkString("[", ",", "]")},${itemset.freq}")
+    }
+*/
     
   }
 }
